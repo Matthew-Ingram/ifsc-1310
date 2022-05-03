fb8b0c7f9c544dcd1f89629d6dba13979050aa47
Matthew-Ingram <mcingram@ualr.edu>
2022-05-03 04:32:51 +0000

    Added slideshow demo to homepage

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.fade').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      slide: 'div',
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 2000
-    });
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
 
 
 });
