afcfcc150f3efd79b397c39f4455b66ea5877a2e
Matthew-Ingram <mcingram@ualr.edu>
2022-05-08 03:12:58 +0000

    Finished majority of revisions on final dragon project

---
+++ dragon-final/assets/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('.slideshow').slick({
+      infinite: true,
+      speed: 750,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+  
+  
+  });
